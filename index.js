4a74f554c9b01451f90fd54200c9dfedded8c43f
minh nguyen <imnotminh@gmail.com>
2019-03-07 06:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ function nestedTarget(){
   return document.querySelector('#nested').querySelector('.target')
 }
 
-function increaseRankBy(n){
-  var rankedL=document.querySelectorAll('.ranked-list');
-  for(var i=0;i<rankedL.length;i++){
-   var li=rankedL[i].querySelectorAll('li');
-   for(var ii=0;ii<li.length;ii++){
-     var num=li[ii].innerHTML;
-     li[ii].innerHTML=parseInt(li[ii].innerHTML)+n
-   }
-  }
-}
+// function increaseRankBy(n){
+//   var rankedL=document.querySelectorAll('.ranked-list');
+//   for(var i=0;i<rankedL.length;i++){
+//   var li=rankedL[i].querySelectorAll('li');
+//   for(var ii=0;ii<li.length;ii++){
+//     var num=li[ii].innerHTML;
+//     li[ii].innerHTML=parseInt(li[ii].innerHTML)+n
+//   }
+//   }
+// }
 
 function deepestChild(){
   var total=document.querySelectorAll('#grand-node div')
