c8855e48915f64cfb10a6e114831697b707f1440
minh nguyen <imnotminh@gmail.com>
2019-03-07 06:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var rankedL=document.querySelectorAll('.ranked-list');
+  for(var i=0;i<rankedL.length;i++){
+   var li=rankedL[i].querySelectorAll('li');
+   for(var ii=0;ii<li.length;ii++){
+     var num=li[ii].innerHTML;
+     li[ii].innerHTML=parseInt(li[ii].innerHTML)+n
+   }
+  }
+}
 
